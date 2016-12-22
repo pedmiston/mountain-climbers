1cf45797e294adff9cd3ed618f1a356382d3dbaa
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-12-21 21:11:52 -0600

    New experiment with solo teams

--- peaks/strategies.py
+++ peaks/strategies.py
@@ -14,10 +14,18 @@ def diachronic(labor_hours, team):
         for calendar_hour in range(i*labor_hours, i*labor_hours+labor_hours):
             yield calendar_hour
 
+
 def alternating(labor_hours, team):
     """Have players take turns."""
     total_hours = labor_hours * len(team.players)
     for calendar_hour in range(total_hours):
         ix = calendar_hour % len(team.players)
         team.active_players = [team.players[ix]]
+        yield calendar_hour
+
+
+def solo(labor_hours, team):
+    assert len(team.players) == 1, 'solo team does not have exactly 1 player'
+    team.active_players = [team.players[0]]
+    for calendar_hour in range(labor_hours * 2):
         yield calendar_hour
