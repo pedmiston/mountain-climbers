66a66cece0ab036558b7fb5f8d51c30ea8b35c66
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-01-01 12:00:59 -0600

    Regression tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -20,3 +20,16 @@ def test_multiple_experiments():
     multiple_experiments_yaml = Path(fixtures_dir, 'multiple-experiments.yaml')
     exp = peaks.Experiment.from_yaml(multiple_experiments_yaml)
     assert len(list(exp)) == 2
+
+def test_specific_starting_positions():
+
+    def make_exp(arg):
+        return peaks.Experiment(data=dict(starting_pos=arg))
+
+    single_pos_exp = make_exp([100, 100]).starting_pos
+    assert len(single_pos_exp) == 1
+    assert single_pos_exp[0] == [100, 100]
+
+    multiple_pos_exp = make_exp([[100, 100], [200, 200]]).starting_pos
+    assert len(multiple_pos_exp) == 2
+    assert multiple_pos_exp[1] == [200, 200]
